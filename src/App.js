0d0ef82811d7faa56c427bdac80fed8fd3de599f
Willdta <beyondutraining@gmail.com>
2018-09-13 17:50:01 +0200

    added token as env key / make api call on enter key press,

--- src/App.js
+++ src/App.js
@@ -6,17 +6,30 @@ class App extends Component {
     searchTerm: '',
     repos: []
   }
-  
+
+  handleChange = e => {
+    const { searchTerm } = this.state
+
+    this.setState({ searchTerm: e.target.value })
+    
+    if (searchTerm === '') {
+      this.setState({ repos: [] })
+    }
+  }
+
   findRepos = () => {
     const { searchTerm } = this.state
 
-    fetch(`https://api.github.com/search/repositories?q=${searchTerm}&per_page=10`)
+    fetch(`https://api.github.com/search/repositories?q=${searchTerm}&per_page=10&access_token=${process.env.REACT_APP_TOKEN}
+`)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        this.setState({ repos: data.items })
+      })
   }
   
   render() {
-    const { searchTerm } = this.state
+    const { searchTerm, repos } = this.state
 
     return (
       <div className="App">
@@ -25,14 +38,28 @@ class App extends Component {
           type="text"
           placeholder="search for a repo..."
           value={searchTerm}
-          onChange={e => this.setState({ searchTerm: e.target.value })}
+          onChange={e => this.handleChange(e)}
+          onKeyPress={e => e.key === 'Enter' && this.findRepos()}
         />
-        <button type="submit" onClick={this.findRepos}>Search</button>
+        <button 
+          type="submit" 
+          onClick={this.findRepos}>
+          Search
+        </button>
 
          <div className="category-container">
           <h5>Name</h5>
           <h5>Language</h5>
-          <h5>Latest Tag</h5>    
+          <h5>Latest Tag</h5>
+
+          {repos.map(repo => (
+            <div key={repo.id}>
+              <a href={repo.html_url}>{repo.full_name}</a>
+              <p>{repo.language}</p>
+              <p>v1</p>
+              <a href="#" onClick={this.addToFavs}>Add</a>
+            </div>
+          ))}   
         </div>
       </div>
     )
