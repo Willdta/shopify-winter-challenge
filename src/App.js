3840f90af46b78cb51e5b4d59c1511faca5592f2
Willdta <beyondutraining@gmail.com>
2018-09-13 11:37:44 +0200

    rough api call / ui setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,38 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  state = {
+    searchTerm: '',
+    repos: []
+  }
+  
+  findRepos = () => {
+    const { searchTerm } = this.state
+
+    fetch(`https://api.github.com/search/repositories?q=${searchTerm}&per_page=10`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+  
   render() {
+    const { searchTerm } = this.state
+
     return (
       <div className="App">
-        
+        <h1>My Github Favorites</h1>
+        <input 
+          type="text"
+          placeholder="search for a repo..."
+          value={searchTerm}
+          onChange={e => this.setState({ searchTerm: e.target.value })}
+        />
+        <button type="submit" onClick={this.findRepos}>Search</button>
+
+         <div className="category-container">
+          <h5>Name</h5>
+          <h5>Language</h5>
+          <h5>Latest Tag</h5>    
+        </div>
       </div>
     )
   }
